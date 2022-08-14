18c1c84497502cc58e1f5a5a57c51eb3a47857ce
Kelly Owoju <kelly.owoju@sterling.ng>
2022-08-14 21:10:22 +0100

    initiated the q-pay web-app and installed initial needed dependecies

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import ReactDOM from "react-dom/client";
+import App from "./App";
+// import { ThemeProvider } from "@material-ui/core/styles";
+// import { themes } from "./store/themes";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+//   <ThemeProvider theme={themes}>
+    <App />
+  /* </ThemeProvider> */
+);
